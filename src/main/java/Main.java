9a0661b45b7b2ceb321f552fe2f04edbbda2f648
dadadaniil <den382505@gmail.com>
2023-05-16 13:58:53 +0300

    in com

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath="C:\\Users\\Dadadaniil\\IdeaProjects\\Practice_Working_With_Strings2\\src\\main\\resources\\in1.properties";
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void getResult(){
+    }
+
 }

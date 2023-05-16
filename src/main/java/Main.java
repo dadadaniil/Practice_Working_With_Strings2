630931d29c695358d89a27c62be9ff632802b58f
dadadaniil <den382505@gmail.com>
2023-05-16 15:03:55 +0300

    erunda

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,42 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Enumeration;
+import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    public static void main(String[] args) {
-        String filePath="C:\\Users\\Dadadaniil\\IdeaProjects\\Practice_Working_With_Strings2\\src\\main\\resources\\in1.properties";
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
+    public static String NAME_OF_FILE = "in%s";
+    public static int ANOUT_OF_FILES = 3;
+
+    public static void main(String[] args) {
+
+
+//        Locale locale = new Locale("en", "US");
+//        ResourceBundle rb = ResourceBundle.getBundle("in", locale);
+//        Enumeration<String> keys = rb.getKeys();
+//        String key;
+//        System.out.println();
+//
+//        while (keys.hasMoreElements()) {
+//            key = keys.nextElement();
+//            System.out.println(key);
+//        }
+        System.out.println("l");
+
+        System.out.println(getValueKey("value 42 =e1"));;
     }
 
-    public static void getResult(){
+    public static String getValueKey(String lineToExtracct) {
+        Pattern pattern = Pattern.compile("value\\s*=\\s*(.*?)=");
+        Matcher matcher = pattern.matcher(lineToExtracct);
+
+        if (matcher.find()) {
+            String extractedString = matcher.group(1).trim();
+            System.out.println(extractedString);
+            return extractedString;
+        }
+        return "nope";
     }
 
 }

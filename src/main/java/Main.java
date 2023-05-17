508267624d8e1421bc6f86f23f17669e75152259
Alena Tsibets <alena.tsibets@gmail.com>
2023-05-17 14:13:37 +0300

    Added tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,47 +1,63 @@
 import java.math.BigDecimal;
 import java.util.Enumeration;
 import java.util.Locale;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
-    private static final String NAME_OF_FILE = "in%s";
+    public static final String NAME_OF_FILE = "in%s";
     private static final int AMOUNT_OF_FILES = 3;
     public static final String INDEX = "index";
     public static final String VALUE = "value";
+    private static final String INDEX_PATTERN = INDEX + "(.+)";
+    private static final String CORRECT_NUMBER_PATTERN = "([1-9])([0-9])*";
+    public static final String SUM_TEXT = "sum = ";
+    public static final String ERRORS_TEXT = "\nerror-lines = ";
+
 
     public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
 
-
-        for (int counter = 1; counter <= AMOUNT_OF_FILES; counter++) {
-            String path = String.format(NAME_OF_FILE, counter);
-            ResourceBundle rb = ResourceBundle.getBundle(path, Locale.ENGLISH);
-            Enumeration<String> keys = rb.getKeys();
-
-            String key;
-            BigDecimal result = new BigDecimal(0);
-            int misunderstandings = 0;
-
-
-            while (keys.hasMoreElements()) {
-                try {
-                    key = keys.nextElement();
-                    if (key.startsWith(INDEX)) {
-                        result = result.add(new BigDecimal((rb.getString(VALUE + getKeyByIndexLine(key, rb)).trim())));
-                    }
-                } catch (Exception wrongArgument) {
-                    misunderstandings++;
-                }
+        try {
+            for (int counter = 1; counter <= AMOUNT_OF_FILES; counter++) {
+                String path = String.format(NAME_OF_FILE, counter);
+                ResourceBundle rb = ResourceBundle.getBundle(path, Locale.ENGLISH);
+                int errors = searchForCorrectValues(builder, rb);
+                builder.append(ERRORS_TEXT).append(errors);
+                builder.append("\n\n");
             }
-
-            System.out.printf("sum = %s%nerror-lines = %d\n%n", result, misunderstandings);
+        } catch (MissingResourceException e) {
+            System.out.println("File not found: " + e);
         }
+        System.out.println(builder);
+    }
+
+    public static int searchForCorrectValues(StringBuilder builder, ResourceBundle bundle) {
+        Enumeration<String> keys = bundle.getKeys();
+
+        String key;
+        BigDecimal result = new BigDecimal(0);
+        int misunderstandings = 0;
+
+        while (keys.hasMoreElements()) {
+            try {
+                key = keys.nextElement();
+                if (key.startsWith(INDEX)) {
+                    result = result.add(new BigDecimal((bundle.getString(VALUE + getKeyByIndexLine(key, bundle)).trim())));
+                }
+            } catch (Exception wrongArgument) {
+                misunderstandings++;
+            }
+        }
+        builder.append(SUM_TEXT).append(result);
+        return misunderstandings;
     }
 
     public static String getKeyByIndexLine(String lineToExtract, ResourceBundle bundle) {
-        Pattern pattern = Pattern.compile(INDEX + "(.+)");
+        Pattern pattern = Pattern.compile(INDEX_PATTERN);
         Matcher matcher = pattern.matcher(lineToExtract);
         String extractedString;
 
@@ -55,7 +71,8 @@ public class Main {
     }
 
     public static String canWeUseThisDigit(String str) {
-        if (!str.matches("([1-9])([0-9])*")) {
+
+        if (!str.matches(CORRECT_NUMBER_PATTERN)) {
             throw new IllegalArgumentException("Wrong format of index or key");
         }
         return str;

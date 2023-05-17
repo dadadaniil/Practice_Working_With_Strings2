905f903ff57de8cf5aa0a1af5930cb167b63a857
dadadaniil <den382505@gmail.com>
2023-05-17 11:12:33 +0300

    non-trivial part is done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.util.Enumeration;
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -7,40 +8,55 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static String NAME_OF_FILE = "in%s";
-    public static int ANOUT_OF_FILES = 3;
+    public static int AMOUNT_OF_FILES = 3;
 
     public static void main(String[] args) {
 
 
-        Locale locale = new Locale("en", "US");
-        ResourceBundle rb = ResourceBundle.getBundle("in", locale);
-        Enumeration<String> keys = rb.getKeys();
-        String key;
-        System.out.println();
+        for (int counter = 1; counter <= AMOUNT_OF_FILES; counter++) {
+            String path = String.format(NAME_OF_FILE, counter);
+            Locale locale = new Locale("en", "US");
+            ResourceBundle rb = ResourceBundle.getBundle(path, locale);
+            Enumeration<String> keys = rb.getKeys();
 
-        while (keys.hasMoreElements()) {
-            key = keys.nextElement();
-            System.out.println(key);
+            String key;
+            BigDecimal result = new BigDecimal(0);
+            int misunderstandings = 0;
+
+
+            while (keys.hasMoreElements()) {
+                try {
+                    key = keys.nextElement();
+                    if (key.startsWith("index")) {
+                        result = result.add(new BigDecimal((rb.getString("value" + getKeyByIndexLine(key, rb)).trim())));
+                    }
+                } catch (Exception wrongArgument) {
+                    misunderstandings++;
+                }
+            }
+
+            System.out.printf("sum = %s%nerror-lines = %d\n%n", result, misunderstandings);
         }
-        System.out.println("l");
-
-        System.out.println(rb.getString("value12"));
-        ;
-
-//        System.out.println(getValueKey("index   1    = 3"));;
     }
 
-    public static String getValueKey(String lineToExtract, ResourceBundle bundle) {
-        Pattern pattern = Pattern.compile("index(.*?)=");
+    public static String getKeyByIndexLine(String lineToExtract, ResourceBundle bundle) {
+        Pattern pattern = Pattern.compile("index(.*)");
         Matcher matcher = pattern.matcher(lineToExtract);
         String extractedString;
 
         if (matcher.find()) {
-            extractedString = matcher.group(1).trim();
-            System.out.println(extractedString); // Output: "1"
-
+            extractedString = canWeUseThisDigit(matcher.group(1).trim());
+            String key = canWeUseThisDigit(bundle.getString("index" + extractedString));
+            return extractedString + key;
+        } else {
+            throw new IllegalArgumentException("Index is missing");
         }
-        return "nope";
+    }
 
+    public static String canWeUseThisDigit(String str) {
+        if (!str.matches("-?\\d+")) {
+            throw new IllegalArgumentException("Wrong format of index or key");
+        }
+        return str;
     }
 }
